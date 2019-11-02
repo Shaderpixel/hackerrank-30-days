cae5aa655e1915df23e4733281ebd6ee050565d9
Richard Lock <lock.j.h@gmail.com>
2019-11-02 12:51:54 -0400

    day 22 solution

---
+++ day-22-binary-search-tree.js
@@ -0,0 +1,59 @@
+// Start of function Node
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+} // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+  this.insert = function(root, data) {
+    if (root === null) {
+      this.root = new Node(data);
+
+      return this.root;
+    }
+
+    if (data <= root.data) {
+      if (root.left) {
+        this.insert(root.left, data);
+      } else {
+        root.left = new Node(data);
+      }
+    } else if (root.right) {
+      this.insert(root.right, data);
+    } else {
+      root.right = new Node(data);
+    }
+
+    return this.root;
+  };
+
+  // Start of function getHeight
+  this.getHeight = function(root) {
+    // Add your code here
+    return 1 + Math.max(root.left !== null ? this.getHeight(root.left) : -1, root.right !== null ? this.getHeight(root.right) : -1);
+  }; // End of function getHeight
+} // End of function BinarySearchTree
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let _input = '';
+
+process.stdin.on('data', (data) => {
+  _input = _input + data;
+});
+
+process.stdin.on('end', () => {
+  let tree = new BinarySearchTree();
+  let root = null;
+
+  let values = _input.split('\n').map(Number);
+
+  for (let i = 1; i < values.length; i++) {
+    root = tree.insert(root, values[i]);
+  }
+
+  console.log(tree.getHeight(root));
+});
